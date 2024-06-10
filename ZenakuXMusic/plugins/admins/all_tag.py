68fefa90cdff658d41e47d075032dfc053774705
Zenaku2050s <yusume65usm@gmail.com>
2024-06-10 21:47:59 +0630

    Update all_tag.py

--- ZenakuXMusic/plugins/admins/all_tag.py
+++ ZenakuXMusic/plugins/admins/all_tag.py
@@ -12,7 +12,7 @@ SPAM_CHATS = []
 
 
 @app.on_message(
-    filters.command(["all", "mention", "mentionall"], prefixes=["/", "@", ".", "#"])
+    filters.command(["all", "mention", "mentionall"], prefixes=["@", "."])
     & admin_filter
 )
 async def tag_all_users(_, message):
@@ -20,19 +20,19 @@ async def tag_all_users(_, message):
     replied = message.reply_to_message
     if len(message.command) < 2 and not replied:
         await message.reply_text(
-            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@all Hi Friends`"
+            "✦✦ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ ✦✦ `@all Hi Friends`"
         )
         return
     if replied:
         SPAM_CHATS.append(message.chat.id)
         usernum = 0
         usertxt = ""
-        async for m in app.get_chat_members(message.user.mention):
-            if message.user.mention not in SPAM_CHATS:
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
-            usertxt += f"\n⊚ {m.user.mention}\n"
-            if usernum == 3:
+            usertxt += f"\n⊚◦•●◉✿ {m.user.mention} ✿◉●•◦\n"
+            if usernum == 5:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(3)
                 usernum = 0
@@ -44,26 +44,27 @@ async def tag_all_users(_, message):
     else:
         text = message.text.split(None, 1)[1]
 
-        SPAM_CHATS.append(message.user.mention)
+        SPAM_CHATS.append(message.chat.id)
         usernum = 0
         usertxt = ""
-        async for m in app.get_chat_members(message.user.mention):
-            if message.user.mention not in SPAM_CHATS:
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
-            usertxt +=f"\n⊚ {m.user.mention}\n"
-            if usernum == 3:
+            usertxt += f"\n◦•●◉✿ {m.user.mention} ✿◉●•◦\n"
+            if usernum == 5:
                 await app.send_message(
-                    message.user.mention,
-                    f"{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
+                    message.chat.id,
+                    f"{text}\n{usertxt}\n\n✦ ι proмιѕe yoυ тнaт ι wιll ѕнare мy lιғe wιтн yoυ...вυт мoѕт ιмporтanт oғ all...ι proмιѕe yoυ тнaт ι wιll love yoυ ғorever...🩵ι wιll love yoυ🩵",
                 )
-                await asyncio.sleep(3)
+                await asyncio.sleep(2)
                 usernum = 0
                 usertxt = ""
         try:
-            SPAM_CHATS.remove(message.user.mention)
+            SPAM_CHATS.remove(message.chat.id)
         except Exception:
             pass
+
 
 
 @app.on_message(
@@ -86,8 +87,8 @@ async def tag_all_users(_, message):
     & admin_filter
 )
 async def cancelcmd(_, message):
-    user_mention = message.user.mention
-    if user_mention in SPAM_CHATS:
+    chat_id = message.chat.id
+    if chat_id in SPAM_CHATS:
         try:
             SPAM_CHATS.remove(chat_id)
         except Exception:
