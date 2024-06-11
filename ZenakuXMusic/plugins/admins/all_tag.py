6ce620e2f8534f652f63c8338473290b98687581
Zenaku2050s <yusume65usm@gmail.com>
2024-06-11 10:05:51 +0630

    Update all_tag.py

--- ZenakuXMusic/plugins/admins/all_tag.py
+++ ZenakuXMusic/plugins/admins/all_tag.py
@@ -31,7 +31,7 @@ async def tag_all_users(_, message):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
-            usertxt += f"🍂 {m.user.mention} "
+            usertxt += f"🫧 {m.user.mention} "
             if usernum == 5:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(3)
@@ -51,7 +51,7 @@ async def tag_all_users(_, message):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
-            usertxt += f"🍂 {m.user.mention} "
+            usertxt += f"🫧 {m.user.mention} "
             if usernum == 5:
                 await app.send_message(
                     message.chat.id,
