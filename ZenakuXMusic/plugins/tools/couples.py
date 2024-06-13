dbc85f3e8f5af55144b1d4556bba9f21ab7b7024
Zenaku2050s <yusume65usm@gmail.com>
2024-06-13 17:36:16 +0630

    Update couples.py

--- ZenakuXMusic/plugins/tools/couples.py
+++ ZenakuXMusic/plugins/tools/couples.py
@@ -1,20 +1,32 @@
+import os 
 import random
-from datetime import datetime
+from datetime import datetime 
+from telegraph import upload_file
+from PIL import Image , ImageDraw
+from pyrogram import *
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import *
 
-from pyrogram import filters
-from pyrogram.enums import ChatType
+#BOT FILE NAME
+from ZenakuXMusic import app as app
+from ZenakuXMusic.mongo.couples_db import _get_image, get_couple
 
-from ZenakuXMusic import pbot
-from ZenakuXMusic.utils.mongo import get_couple, save_couple
+POLICE = [
+    [
+        InlineKeyboardButton(
+            text="💞 ᴄᴜᴛᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ 💞",
+            url=f"https://t.me/NhoeKyaiteKaungLayy",
+        ),
+    ],
+]
 
 
-# Date and time
 def dt():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M")
     dt_list = dt_string.split(" ")
     return dt_list
-
+    
 
 def dt_tom():
     a = (
@@ -26,59 +38,130 @@ def dt_tom():
     )
     return a
 
-
-today = str(dt()[0])
 tomorrow = str(dt_tom())
+today = str(dt()[0])
 
-
-@pbot.on_message(filters.command(["couple", "couples"]))
-async def couple(_, message):
+@app.on_message(filters.command("couple", "couples", "lover", "လင်နိ", "မယားနိ", "ကောင်လေးနိ", "ကောင်မလေးနိ", "ညမလေးနိ", "မောင်လေးနိ", "မမနိ", "ကိုကိုနိ"))
+async def ctest(_, message):
+    cid = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
-        return await message.reply_text("This command only works in groups.")
+        return await message.reply_text("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋs ɪɴ ɢʀᴏᴜᴘs.")
     try:
-        chat_id = message.chat.id
-        is_selected = await get_couple(chat_id, today)
-        if not is_selected:
-            list_of_users = []
-            async for i in pbot.get_chat_members(message.chat.id, limit=50):
-                if not i.user.is_bot:
-                    list_of_users.append(i.user.id)
-            if len(list_of_users) < 2:
-                return await message.reply_text("Not enough users")
-            c1_id = random.choice(list_of_users)
-            c2_id = random.choice(list_of_users)
-            while c1_id == c2_id:
-                c1_id = random.choice(list_of_users)
-            c1_mention = (await pbot.get_users(c1_id)).mention
-            c2_mention = (await pbot.get_users(c2_id)).mention
+     #  is_selected = await get_couple(cid, today)
+     #  if not is_selected:
+         msg = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴏᴜᴘʟᴇs ɪᴍᴀɢᴇ...")
+         #GET LIST OF USERS
+         list_of_users = []
 
-            couple_selection_message = f"""**Couple of the day :**
+         async for i in app.get_chat_members(message.chat.id, limit=10000):
+             if not i.user.is_bot:
+               list_of_users.append(i.user.id)
 
-{c1_mention} + {c2_mention} = 🩵
-__New couple of the day can be chosen at 12AM {tomorrow}__"""
-            await pbot.send_message(message.chat.id, text=couple_selection_message)
-            couple = {"c1_id": c1_id, "c2_id": c2_id}
-            await save_couple(chat_id, today, couple)
+         c1_id = random.choice(list_of_users)
+         c2_id = random.choice(list_of_users)
+         while c1_id == c2_id:
+              c1_id = random.choice(list_of_users)
 
-        elif is_selected:
-            c1_id = int(is_selected["c1_id"])
-            c2_id = int(is_selected["c2_id"])
-            c1_name = (await pbot.get_users(c1_id)).mention
-            c2_name = (await pbot.get_users(c2_id)).mention
-            couple_selection_message = f"""Couple of the day :
 
-{c1_name} + {c2_name} = 🩵
-__New couple of the day can be chosen at 12AM {tomorrow}__"""
-            await pbot.send_message(message.chat.id, text=couple_selection_message)
+         photo1 = (await app.get_chat(c1_id)).photo
+         photo2 = (await app.get_chat(c2_id)).photo
+ 
+         N1 = (await app.get_users(c1_id)).mention 
+         N2 = (await app.get_users(c2_id)).mention
+         
+         try:
+            p1 = await app.download_media(photo1.big_file_id, file_name="pfp.png")
+         except Exception:
+            p1 = "ZenakuXMusic/assets/upic.png"
+         try:
+            p2 = await app.download_media(photo2.big_file_id, file_name="pfp1.png")
+         except Exception:
+            p2 = "ZenakuXMusic/assets/upic.png"
+            
+         img1 = Image.open(f"{p1}")
+         img2 = Image.open(f"{p2}")
+
+         img = Image.open("ZenakuXMusic/assets/zenakucouple12k.jpg")
+
+         img1 = img1.resize((437,437))
+         img2 = img2.resize((437,437))
+
+         mask = Image.new('L', img1.size, 0)
+         draw = ImageDraw.Draw(mask) 
+         draw.ellipse((0, 0) + img1.size, fill=255)
+
+         mask1 = Image.new('L', img2.size, 0)
+         draw = ImageDraw.Draw(mask1) 
+         draw.ellipse((0, 0) + img2.size, fill=255)
+
+
+         img1.putalpha(mask)
+         img2.putalpha(mask1)
+
+         draw = ImageDraw.Draw(img)
+
+         img.paste(img1, (116, 160), img1)
+         img.paste(img2, (789, 160), img2)
+
+         img.save(f'test_{cid}.png')
+    
+         TXT = f"""
+🫧 ᴛᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ 🫧:
+
+{N1} + {N2} = 🩵
+
+🫧ɴᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow} 🫧
+"""
+    
+         await message.reply_photo(f"test_{cid}.png", caption=TXT, reply_markup=InlineKeyboardMarkup(POLICE),
+    )
+         await msg.delete()
+         a = upload_file(f"test_{cid}.png")
+         for x in a:
+           img = "https://graph.org/" + x
+           couple = {"c1_id": c1_id, "c2_id": c2_id}
+          # await save_couple(cid, today, couple, img)
+    
+         
+      # elif is_selected:
+      #   msg = await message.reply_text("𝐆ᴇᴛᴛɪɴɢ 𝐓ᴏᴅᴀʏs 𝐂ᴏᴜᴘʟᴇs 𝐈ᴍᴀɢᴇ...")
+      #   b = await _get_image(cid)
+       #  c1_id = int(is_selected["c1_id"])
+       #  c2_id = int(is_selected["c2_id"])
+       #  c1_name = (await app.get_users(c1_id)).first_name
+        # c2_name = (await app.get_users(c2_id)).first_name
+         
+      #   TXT = f"""
+#**ᴛᴏᴅᴀʏ's sᴇʟᴇᴄᴛᴇᴅ ᴄᴏᴜᴘʟᴇs 🎉 :
+#➖➖➖➖➖➖➖➖➖➖➖➖
+#[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❣️
+#➖➖➖➖➖➖➖➖➖➖➖➖
+#ɴᴇxᴛ ᴄᴏᴜᴘʟᴇꜱ ᴡɪʟʟ ʙᴇ ꜱᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow} !!**
+#"""
+ #        await message.reply_photo(b, caption=TXT)
+        # await msg.delete()
     except Exception as e:
-        print(e)
-        await message.reply_text(e)
+        print(str(e))
+    try:
+      os.remove(f"./downloads/pfp1.png")
+      os.remove(f"./downloads/pfp2.png")
+      os.remove(f"test_{cid}.png")
+    except Exception:
+       pass
+         
 
-
+__mod__ = "COUPLES"
 __help__ = """
-Choose couples in your chat
-
- ❍ /couple *:* Choose 2 users and send their name as couples in your chat.
+🫧/couples*:* - Get Todays Couples Of The Group In Interactive View
 """
 
-__mod_name__ = "Cᴏᴜᴘʟᴇ"
+
+
+
+
+    
+
+
+
+
+    
