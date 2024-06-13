0d48b7575864050d16e357520419be3ef31f4b63
Zenaku2050s <yusume65usm@gmail.com>
2024-06-13 19:16:11 +0630

    Update couples.py

--- ZenakuXMusic/plugins/tools/couples.py
+++ ZenakuXMusic/plugins/tools/couples.py
@@ -41,7 +41,7 @@ def dt_tom():
 tomorrow = str(dt_tom())
 today = str(dt()[0])
 
-@app.on_message(filters.command("couple", "couples", "lover", "လင်နိ", "မယားနိ", "ကောင်လေးနိ", "ကောင်မလေးနိ", "ညမလေးနိ", "မောင်လေးနိ", "မမနိ", "ကိုကိုနိ"))
+@app.on_message(filters.command("couple", "couples"))
 async def ctest(_, message):
     cid = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
