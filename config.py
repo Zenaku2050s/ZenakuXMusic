4a09ab9b0235e89c841b526f17feee2d2fd541e0
Zenaku2050s <yusume65usm@gmail.com>
2024-07-18 10:43:04 +0630

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+KOYEB_API_KEY = getenv("KOYEB_API_KEY")
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Zenaku2050s/ZenakuXMusic",
