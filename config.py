cd152e4808d7ee7eaf64830852cb0c14a065b9d5
Zenaku2050s <yusume65usm@gmail.com>
2024-08-06 14:37:58 +0630

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002055407973"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002011820973"))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 1318826936))
